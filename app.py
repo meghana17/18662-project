05f9e1abe9cf1f354d429bd9b83f83d98f362eff
Meghana Balarama <meghana1704@gmail.com>
2022-04-27 20:41:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ with st.form(key="my_form", clear_on_submit=True):
     scores = model(**features).logits
     label_mapping = ['REFUTED', 'SUPPORTED', 'NEI']
     labels = [label_mapping[score_max] for score_max in scores.argmax(dim=1)]
+    st.write(claim)
+    st.write(evidence)
+    st.write(labels)
     if labels=='SUPPORTED':
       st.write('The evidence SUPPORTS the claim')
     if labels=='REFUTED':
