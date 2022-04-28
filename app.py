714ca82bddb09f592b998d1631343f6c9c6c31f7
Meghana Balarama <meghana1704@gmail.com>
2022-04-27 20:56:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,22 +19,23 @@ with st.form(key="my_form", clear_on_submit=True):
   evidence = st.text_input("Evidence")
   
   submitted = st.form_submit_button(label="Verify")
-  
-  model = AutoModelForSequenceClassification.from_pretrained("amandakonet/climatebert-fact-checking")
-  tokenizer = AutoTokenizer.from_pretrained("amandakonet/climatebert-fact-checking")
-  features = tokenizer([str(claim)],[str(evidence)], padding='max_length', truncation=True, return_tensors="pt", max_length=512)
-                     
-  model.eval()
-  with torch.no_grad():
-    scores = model(**features).logits
-    label_mapping = ['REFUTED', 'SUPPORTED', 'NEI']
-    labels = [label_mapping[score_max] for score_max in scores.argmax(dim=1)]
-    st.write("CLAIM:", claim)
-    st.write("EVIDENCE:", evidence)
-    
-    if labels[0]=='SUPPORTED':
-      st.write('The evidence SUPPORTS the claim')
-    if labels[0]=='REFUTED':
-      st.write('The evidence REFUTES the claim')
-    if labels[0]=='NEI':
-      st.write('There isn\'t enough information to verify this claim')
+
+  if submitted:
+      model = AutoModelForSequenceClassification.from_pretrained("amandakonet/climatebert-fact-checking")
+      tokenizer = AutoTokenizer.from_pretrained("amandakonet/climatebert-fact-checking")
+      features = tokenizer([str(claim)],[str(evidence)], padding='max_length', truncation=True, return_tensors="pt", max_length=512)
+
+      model.eval()
+      with torch.no_grad():
+        scores = model(**features).logits
+        label_mapping = ['REFUTED', 'SUPPORTED', 'NEI']
+        labels = [label_mapping[score_max] for score_max in scores.argmax(dim=1)]
+        st.write("CLAIM:", claim)
+        st.write("EVIDENCE:", evidence)
+
+        if labels[0]=='SUPPORTED':
+          st.write('The evidence SUPPORTS the claim')
+        if labels[0]=='REFUTED':
+          st.write('The evidence REFUTES the claim')
+        if labels[0]=='NEI':
+          st.write('There isn\'t enough information to verify this claim')
